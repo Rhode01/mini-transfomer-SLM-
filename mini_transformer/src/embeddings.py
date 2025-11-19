96c45649b540f4c8180435e06eb398a2d07a4b3c
Rhode01 <Rhodrickmhone@gmail.com>
2025-11-19 14:41:53 +0200

    .

--- mini_transformer/src/embeddings.py
+++ mini_transformer/src/embeddings.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import math
 
 class WordEmbedding(nn.Module):
     def __init__(self, vocab_size, embed_dim):
@@ -8,3 +9,19 @@ class WordEmbedding(nn.Module):
 
     def forward(self, x):
         return self.embedding(x) 
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, embed_dim, max_len=100):
+        super().__init__()
+        self.pe = torch.zeros(max_len, embed_dim)
+        self.position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        self.div_term = torch.exp(torch.arange(0, embed_dim, 2).float() * (-math.log(10000.0) / embed_dim))
+        self.pe[:, 0::2] = torch.sin(self.position * self.div_term)
+        self.pe[:, 1::2] = torch.cos(self.position * self.div_term)
+        self.pe = self.pe.unsqueeze(0) 
+        self.register_buffer('pe', self.pe)
+
+    def forward(self, x):
+        seq_len = x.size(1)
+        x = x + self.pe[:, :seq_len, :]
+        return x
